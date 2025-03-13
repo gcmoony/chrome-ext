934a687ebdf1d7338331ce16749c76cb1da42de4
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-03-13 10:20:27 +0900

    Added MUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
 import { useState } from "react"
+import CssBaseline from "@mui/material/CssBaseline"
+import { Box, Container } from "@mui/material"
+import { ThemeProvider, createTheme } from "@mui/material/styles"
 
 function App() {
+  const theme = createTheme({
+    colorSchemes: {
+      dark: true,
+    },
+  })
   return (
     <>
-      <h1>Hello World</h1>
+      <ThemeProvider
+        theme={theme}
+        noSsr
+      >
+        <CssBaseline enableColorScheme={true} />
+        <Container sx={{}}>
+          <Box sx={{ minHeight: "95vh" }}>
+            <h1>Hello World</h1>
+          </Box>
+        </Container>
+      </ThemeProvider>
     </>
   )
 }
